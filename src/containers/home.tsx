2feec64f03e0009f6b17c510c6c4cb90f6d594f7
Minh Tri <46084113+ume16@users.noreply.github.com>
2022-12-05 23:40:06 +0700

    full models uploaded

--- src/containers/home.tsx
+++ src/containers/home.tsx
@@ -9,6 +9,7 @@ const HomePage = () => {
     const [predict, setPredict] = useState(false);
     const [salganUrl, setsalganUrl] = useState<string>("");
     const [transalnetUrl, settransalnetUrl] = useState<string>("");
+    const [msinetUrl, setmsinetUrl] = useState<string>("");
     const [previewUrl, setPreviewUrl] = useState<string>("");
     const [notification, setNotification] = useState<number>(0);
 
@@ -19,19 +20,19 @@ const HomePage = () => {
                 "p-4 text-yellow-900 bg-yellow-100 border border-yellow-200 rounded-md",
             title: "Attention",
             img: "M13.6086 3.247l8.1916 15.8c.0999.2.1998.5.1998.8 0 1-.7992 1.8-1.7982 1.8H3.7188c-.2997 0-.4995-.1-.7992-.2-.7992-.5-1.1988-1.5-.6993-2.4 5.3067-10.1184 8.0706-15.385 8.2915-15.8.3314-.6222.8681-.8886 1.4817-.897.6135-.008 1.273.2807 1.6151.897zM12 18.95c.718 0 1.3-.582 1.3-1.3 0-.718-.582-1.3-1.3-1.3-.718 0-1.3.582-1.3 1.3 0 .718.582 1.3 1.3 1.3zm-.8895-10.203v5.4c0 .5.4.9.9.9s.9-.4.9-.9v-5.3c0-.5-.4-.9-.9-.9s-.9.4-.9.8z",
-            content: "Only TranSalNet and SalGAN are working!",
+            content: "Since the server's resource is limited, it might take seconds to work!",
         },
         pending: {
             style: "p-4 text-blue-900 bg-blue-100 border border-blue-200 rounded-md",
             title: "Pending",
             img: "M14.1667 17h-3.3334c-.5 0-.8333-.3146-.8333-.7865 0-.472.3333-.7865.8333-.7865H11.5c.0833 0 .1667-.0787.1667-.1573v-3.5394c0-.0786-.0834-.1573-.1667-.1573h-.6667c-.5 0-.8333-.3146-.8333-.7865S10.3333 10 10.8333 10h.8334c.9166 0 1.6666.7079 1.6666 1.573v3.7753c0 .0787.0834.1573.1667.1573h.6667c.5 0 .8333.3146.8333.7865 0 .472-.3333.7079-.8333.7079zM12.3 6c.6933 0 1.3.6067 1.3 1.3s-.52 1.3-1.3 1.3S11 7.9933 11 7.3 11.6067 6 12.3 6zM12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2",
-            content: "Image is processing!",
+            content: "Image is being handled!",
         },
         success: {
             style: "p-4 text-green-900 bg-green-100 border border-green-200 rounded-md",
             title: "Success",
             img: "M8.445 12.6675A.9.9 0 0 0 7.1424 13.91l2.5726 2.7448c.3679.3856.9884.3689 1.335-.036l5.591-7.0366a.9.9 0 0 0-1.3674-1.1705l-4.6548 5.9132a.4.4 0 0 1-.607.0252l-1.567-1.6826zM1.9995 12c0-5.5 4.5-10 10-10s10 4.5 10 10-4.5 10-10 10-10-4.5-10-10z",
-            content: "Task completed successfully!",
+            content: "Image handled successfully!",
         },
     };
 
@@ -64,9 +65,9 @@ const HomePage = () => {
                 e.target.files ? setPreviewUrl(URL.createObjectURL(e.target.files[0])) : console.log(`file is null`);
                 setPreview(true);
             })
-            .catch((e) => console.log("Error at uploading with", e));
+            .catch((e) => console.log("Error while uploading image", e));
 
-        await axios
+        let tranSalNetResult = axios
             .post<File>("https://minzi.live/transalnet", data, {
                 headers: {
                     "Content-Type": "multipart/form-data",
@@ -76,7 +77,21 @@ const HomePage = () => {
             .then((res) => {
                 settransalnetUrl(URL.createObjectURL(res.data));
             })
-            .catch((e) => console.log("Error at transalnet with", e));
+            .catch((e) => console.log("Error at TranSalNet with", e));
+
+        let msiNetResult = axios
+            .post<File>("https://minzi.live/msinet", data, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+                responseType: "blob",
+            })
+            .then((res) => {
+                setmsinetUrl(URL.createObjectURL(res.data));
+            })
+            .catch((e) => console.log("Error at MSINet with", e));
+
+        await Promise.all([tranSalNetResult, msiNetResult]);
     };
 
     const getPrediction = async (): Promise<void> => {
@@ -98,9 +113,9 @@ const HomePage = () => {
 
     const navigation = [
         { name: "Dashboard", href: "", current: true },
-        { name: "TranSalNet", href: "#", current: false },
-        { name: "CASNet", href: "#", current: false },
-        { name: "SalGAN", href: "#", current: false },
+        // { name: "TranSalNet", href: "#", current: false },
+        // { name: "MSINet", href: "#", current: false },
+        // { name: "SalGAN", href: "#", current: false },
     ];
 
     function classNames(...classes: string[]) {
@@ -256,13 +271,13 @@ const HomePage = () => {
                             <div className="flex text-center justify-center align-middle h-fit">
                                 <h1 className="m-auto mb-4 text-3xl font-extrabold dark:text-white md:text-3xl lg:text-4xl">
                                     <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-                                        ORIGINAL IMAGE RESIZED TO 4:3 RATIO
+                                        UPLOADED IMAGE
                                     </span>
                                 </h1>
                             </div>
                         ) : (
                             <div className="flex text-center justify-center align-middle h-fit">
-                                <h1 className="mt-20 text-5xl font-extrabold text-white dark:text-white md:text-5xl lg:text-6xl">
+                                <h1 className="mt-16 text-5xl font-extrabold text-white dark:text-white md:text-5xl lg:text-6xl">
                                     START BY <br></br>
                                     <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
                                         UPLOADING AN IMAGE
@@ -294,12 +309,12 @@ const HomePage = () => {
                             />
                         ) : null}
 
-                        {/* {predict ? (
+                        {predict ? (
                             <SaliencyResult
-                                imageUrl={preview || ""}
-                                caption="CASNET SALIENCY MAP RESULT"
+                                imageUrl={msinetUrl || ""}
+                                caption="MSI SALIENCY MAP RESULT"
                             />
-                        ) : null} */}
+                        ) : null}
 
                         {predict ? (
                             <SaliencyResult
